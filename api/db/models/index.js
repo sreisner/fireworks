cb0d9101a9f1634b0049060245f65102d5a2cf8d
sreisner <shawn.reisner@gmail.com>
2018-08-07 16:37:58 -0500

    Remove unused bundle code

--- api/db/models/index.js
+++ api/db/models/index.js
@@ -1,11 +1,9 @@
 const Product = require('./product');
 const User = require('./user');
-const Bundle = require('./bundle');
 const Category = require('./category');
 
 module.exports = {
   Product,
   User,
-  Bundle,
   Category,
 };
