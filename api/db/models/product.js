15caf1ac02535c7daa946f17b179d0ab5fbcc38f
sreisner <shawn.reisner@gmail.com>
2018-08-07 16:37:46 -0500

    Update product mongoose schema

--- api/db/models/product.js
+++ api/db/models/product.js
@@ -1,6 +1,66 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ProductSchema = Schema({}, { collection: 'products' });
+const ProductSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    categoryId: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrls: {
+      type: [String],
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: false,
+    },
+    retailPrice: {
+      type: Number,
+      required: false,
+    },
+    casePrice: {
+      type: Number,
+      required: true,
+    },
+    manufacturer: {
+      type: String,
+      required: false,
+    },
+    colors: {
+      type: [String],
+      required: false,
+    },
+    videoUrl: {
+      type: String,
+      required: false,
+    },
+    type: {
+      type: String,
+      required: false,
+    },
+    numShots: {
+      type: Number,
+      required: false,
+    },
+    casePacking: {
+      type: Number,
+      required: true,
+    },
+    effects: {
+      type: [String],
+      required: false,
+    },
+  },
+  { collection: 'products' }
+);
 
 module.exports = mongoose.model('Product', ProductSchema);
