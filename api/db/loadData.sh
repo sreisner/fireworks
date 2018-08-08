fdd0ab874d63ac9cc17df1c7184cf297a9a2714d
sreisner <shawn.reisner@gmail.com>
2018-08-08 15:02:26 -0500

    Remove bundle loading into db

--- api/db/loadData.sh
+++ api/db/loadData.sh
@@ -2,4 +2,3 @@
 
 mongoimport --db fireworks --collection categories --file ./data/categories.json --jsonArray;
 mongoimport --db fireworks --collection products --file ./data/products.json --jsonArray;
-mongoimport --db fireworks --collection bundles --file ./data/bundles.json --jsonArray;
