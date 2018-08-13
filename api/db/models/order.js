a99e6c4be7f505870f7a03edac270ba185e13891
sreisner <shawn.reisner@gmail.com>
2018-08-12 19:45:31 -0500

    Add missing order module

---
+++ api/db/models/order.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const OrderProduct = Schema({
+  count: {
+    type: Number,
+    required: true,
+  },
+  productId: {
+    type: Number,
+    required: true,
+  },
+});
+
+const OrderSchema = Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    zip: {
+      type: Number,
+      required: true,
+    },
+    products: {
+      type: [OrderProduct],
+      required: true,
+    },
+    amountCharged: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    createdOn: {
+      type: Date,
+      required: true,
+      default: new Date(),
+    },
+  },
+  { collection: 'orders' }
+);
+
+module.exports = mongoose.model('Order', OrderSchema);
